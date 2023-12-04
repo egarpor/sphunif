05f70f360dd5eddfc91a19358034c96dd1398230
Eduardo García-Portugués <egarpor@users.noreply.github.com>
2023-12-04 16:38:37 +0100

    Add doRNG::`%dorng%`

--- R/int_sph_MC.R
+++ R/int_sph_MC.R
@@ -91,15 +91,13 @@ int_sph_MC <- function(f, p, M = 1e4, cores = 1, chunks = ceiling(M / 1e3),
   # Parallel backend
   old_dopar <- doFuture::registerDoFuture()
   old_plan <- future::plan(future::multisession(), workers = cores)
-  options(future.rng.onMisuse = "ignore")
   on.exit({
 
     with(old_dopar, foreach::setDoPar(fun = fun, data = data, info = info))
     future::plan(old_plan)
-    options(future.rng.onMisuse = NULL)
 
   })
-  `%op%` <- foreach::`%dopar%`
+  `%op%` <- doRNG::`%dorng%`
 
   # Measure progress?
   if (requireNamespace("progressr", quietly = TRUE)) {

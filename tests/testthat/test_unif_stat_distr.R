60ee672b98bc9971941494ee6482eb70009e7b92
Eduardo García-Portugués <egarpor@users.noreply.github.com>
2024-05-10 23:48:15 +0200

    Fix bug in crit_val with vectorized statistivs

--- tests/testthat/test_unif_stat_distr.R
+++ tests/testthat/test_unif_stat_distr.R
@@ -59,7 +59,7 @@ test_that("Statistic arguments of unif_stat_distr are contained in those of
           unif_test", {
 
   expect_true(all(
-    names(formals(unif_stat_distr))[-c(1:7, 20:30)] %in%
+    names(formals(unif_stat_distr))[-c(1:7, 10, 16, 18:20)] %in%
       names(formals(unif_test))[-c(1:8)]
   ))
 
